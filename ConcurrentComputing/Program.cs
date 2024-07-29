97259144e827fe564ac8671bd45e1b69ed2f8a8f
Ray <raytsang21@gmail.com>
2024-07-29 09:48:20 -0700

    first commit

---
+++ ConcurrentComputing/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Parallel computing!");
+
+Parallel.For(0, 10, i =>
+{
+    Console.WriteLine($"Processing {i}");
+});
+
+
---
+++ ParallelcomputingNunit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ParallelcomputingNunit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
