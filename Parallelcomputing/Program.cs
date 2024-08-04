9451e40c57a129bf87f98689a6bdb44f50842e29
Ray <raytsang21@gmail.com>
2024-08-04 12:49:29 -0700

    modify project

--- Parallelcomputing/Program.cs
+++ Parallelcomputing/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("testing");
+Console.WriteLine("mvoed .gitignor to the root of the solution folder");
